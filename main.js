a1374ac122592997d478549b97dc087c93fca0a4
stevenharders <stevenharders@hks.harvard.edu>
2023-01-06 18:47:01 -0500

    change to alert when click fox

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-const myImage = document.querySelector("img");
-
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "firefox-image.png") {
-    myImage.setAttribute("src", "firefox2.png");
-  } else {
-    myImage.setAttribute("src", "firefox-image.png");
-  }
-};
+ function sayOuch() {
+  alert('Ouch! Stop poking me!');
+ }
+ var foxImage = document.querySelector('img');
+foxImage.onclick = sayOuch;
 
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
